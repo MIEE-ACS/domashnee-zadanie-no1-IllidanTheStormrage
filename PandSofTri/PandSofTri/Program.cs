824c54a6dbe9f19e5be74cd4c34b478f34699621
IllidanTheStormrage <1ll1dan.th3st0rmrag3@gmail.com>
2021-09-18 14:10:52 +0300

    Сменил тип данных, + многократная проверка

--- PandSofTri/PandSofTri/Program.cs
+++ PandSofTri/PandSofTri/Program.cs
@@ -6,15 +6,18 @@ namespace PandSofTri
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Для вычисления периметра и площади прямоугольника, поочерёдно введите координаты его вершин:");
-            Console.WriteLine("Сначала для первой точки:");
-            float x1 = float.Parse(Console.ReadLine());
-            float y1 = float.Parse(Console.ReadLine());
-            Console.WriteLine("...Потом для второй:");
-            float x2 = float.Parse(Console.ReadLine());
-            float y2 = float.Parse(Console.ReadLine());
-            float per = 2*(Math.Abs(x1 - x2)+Math.Abs(y1 - y2));
-            float sq = Math.Abs(x1 - x2)*Math.Abs(y1 - y2);
+            Console.WriteLine("Для вычисления периметра и площади прямоугольника, поочерёдно введите координаты его вершин.\nСначала для первой точки:\nX:");
+            double x1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Y:");
+            double y1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("...Потом для второй:\nX:");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Y:");
+            double y2 = double.Parse(Console.ReadLine());
+            double per = 2*(Math.Abs(x1 - x2)+Math.Abs(y1 - y2));
+            per = Math.Round(per, 3);
+            double sq = Math.Abs(x1 - x2)*Math.Abs(y1 - y2);
+            sq = Math.Round(sq, 3);
             Console.WriteLine($"Периметр для прямоугольника, с введёнными координатами вершин: {per}; площадь: {sq}.");
         }
     }
